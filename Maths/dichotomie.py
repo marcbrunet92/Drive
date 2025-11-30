66c6d1ea9cbee57d03f98c4c9f30bb25e55ec29b
Marc Brunet <marcbrunet92@gmail.com>
2025-11-30 12:05:47 +0100

    update

--- Maths/dichotomie.py
+++ Maths/dichotomie.py
@@ -1,14 +1,19 @@
+# fonction à analyser
 def f(x:float) -> float:
     return x**3+x
 
 def dich_recursive(a:float, b:float, f, p:float) -> float:
+    # Verification des entrées
     if p <= 0:
         raise ValueError(f"la valeur de p : {p} doit être strictement positive")
     if f(a)*f(b) > 0:
         raise ValueError(f"f({a})*f({b}) n'est pas négatif")
+
+    # algorithme dichotomique
     c = (a+b)/2
     if abs(a-b)<p:
         return c
     if f(c)-f(a)<=0:
         return dich(a, c, f, p)
     return dich(c, b, f, p)
+
